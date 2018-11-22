e751fbb58ae8fe30123396c51a51236b596f5085
Gal Ifrah <gal.ifrahh@gmail.com>
2018-11-22 23:36:18 +0200

    adding params from config

--- FrancoManca/FmTests.py
+++ FrancoManca/FmTests.py
@@ -10,15 +10,21 @@ html_report_dir = 'C:/Users/MyCheck/PycharmProjects/WebAutomation/Reports'
 
 class FmTestsClass(BasicTestClass, unittest.TestCase):
     def test_100_OpenFm(self):
+        FmHomePage.printConfig()
         FmHomePage.openLoginPage()
         FmHomePage.clickOnCookPolicyBtn()
         FmHomePage.clickOnConnect()
-        EnterPhonePage.enterValidPhoneNumber("+972542567405")
+        EnterPhonePage.enterValidPhoneNumber()
         EnterPhonePage.clickOnSubmitBtn()
         EnterPhonePage.enterSmsCode()
         EnterPhonePage.submitSmsCode()
         EnterPhonePage.enterEmail('galgal12345@gmail.com')
         EnterPhonePage.submitEmail()
+        EnterPhonePage.enterFullName("gal tester")
+        EnterPhonePage.enterPin(1234)
+        EnterPhonePage.enterDate("01/02/2003")
+        EnterPhonePage.chooseOptinTrue()
+        EnterPhonePage.submitForm()
 
 
 if __name__ == "__main__":
--- FrancoManca/PageObjects.py
+++ FrancoManca/PageObjects.py
@@ -1,3 +1,5 @@
+import json
+
 from Infrastructure.GenericPageObject import GenericPO
 from Infrastructure.Locators import LocatorsTypes
 
@@ -16,31 +18,40 @@ ENTER_SMS_CODE = '//form/div/div/input'
 SUBMIT_SMS_CODE = '//form/div/div/div'
 ENTER_EMAIL_FIELD = '//input[@type="email"]'
 SUBMIT_EMAIL_BUTTON = '//div[@type="button"]'
-
-
+FORM_EMAIL_FIELD = ''
+FORM_FULL_NAME_FIELD = '//div[@class="col-12"]/input[@placeholder="Full Name"]'
+FORM_PIN_FIELD = '//div[@class="relative-wrapper"]/input[@placeholder="Choose your own 4 digit passcode"]'
+FORM_DATE_FIELD = '//div[@class="relative-wrapper"]/input[@placeholder="Date Of Birth"]'
+FORM_OPTIN_TRUE = '/html/body/div[1]/div/div/div/div/div[2]/div/ng-include/div/div/form/div[5]/div/label'
+FORM_SUBMIT_BUTTON = '//div[@class="continue-btn item-input-disable ng-binding"]'
 # python run-tests.py --config=/config/apps/franco.json
+
 
 class FmHomePage(GenericPO):
 
     @staticmethod
+    def printConfig():
+        print(json.dumps(params, indent=4))
+
+    @staticmethod
     def openLoginPage():
-        GenericPO.webDriver.openSut(params['SUT']['prod'])
+        GenericPO.webDriver.openSut(params['SUT']['test'])
 
     @staticmethod
     def clickOnCookPolicyBtn():
-        GenericPO.webDriver.findElementBy(COOKIES_POLICY_BTN, LocatorsType=LocatorsTypes.XPATH).click()
+        GenericPO.webDriver.findElementBy(params['HOME_PAGE']['COOKIES_POLICY_BTN'], LocatorsType=LocatorsTypes.XPATH).click()
 
     @staticmethod
     def backToFmFromHeader():
-        GenericPO.webDriver.findElementBy(BACK_TO_FM_HEADER, LocatorsType=LocatorsTypes.XPATH).click()
+        GenericPO.webDriver.findElementBy(params['HOME_PAGE']['BACK_TO_APP_HEADER'], LocatorsType=LocatorsTypes.XPATH).click()
 
     @staticmethod
     def backToFmFromLogo():
-        GenericPO.webDriver.findElementBy(BACK_TO_FM_LOGO, LocatorsType=LocatorsTypes.XPATH).click()
+        GenericPO.webDriver.findElementBy(params['HOME_PAGE']['BACK_TO_APP_LOGO'], LocatorsType=LocatorsTypes.XPATH).click()
 
     @staticmethod
     def clickOnConnect():
-        GenericPO.webDriver.findElementBy(CONNECT_BTN, LocatorsType=LocatorsTypes.XPATH).click()
+        GenericPO.webDriver.findElementBy(params['HOME_PAGE']['CONNECT_BTN'], LocatorsType=LocatorsTypes.XPATH).click()
 
 
 # end of login page class
@@ -51,16 +62,18 @@ class FmHomePage(GenericPO):
 class EnterPhonePage(GenericPO):
 
     @staticmethod
-    def enterValidPhoneNumber(phone):
-        GenericPO.webDriver.findElementBy(PHONE_FIELD, LocatorsType=LocatorsTypes.XPATH).send_keys(phone)
+    def enterValidPhoneNumber():
+        GenericPO.webDriver.findElementBy(params['ENTER_PHONE_PAGE']['LOCATORS']['PHONE_FIELD'], LocatorsType=
+                            LocatorsTypes.XPATH).send_keys(params['ENTER_PHONE_PAGE']['DATA']['VALID_PHONE_NUMBER'])
 
     @staticmethod
     def clickOnSubmitBtn():
-        GenericPO.webDriver.findElementBy(SUBMIT_BUTTON, LocatorsType=LocatorsTypes.XPATH).click()
+        GenericPO.webDriver.findElementBy(params['ENTER_PHONE_PAGE']['LOCATORS']['SUBMIT_BUTTON'], LocatorsType=
+                            LocatorsTypes.XPATH).click()
 
     @staticmethod
     def enterSmsCode():
-        print "enter sms code"
+        print("enter sms code")
         code = input()
         GenericPO.webDriver.findElementBy(ENTER_SMS_CODE, LocatorsType=LocatorsTypes.XPATH).send_keys(code)
 
@@ -76,7 +89,25 @@ class EnterPhonePage(GenericPO):
     def submitEmail():
         GenericPO.webDriver.findElementBy(SUBMIT_EMAIL_BUTTON, LocatorsType=LocatorsTypes.XPATH).click()
 
+    @staticmethod
+    def enterFullName(name):
+        GenericPO.webDriver.findElementBy(FORM_FULL_NAME_FIELD, LocatorsType=LocatorsTypes.XPATH).send_keys(name)
 
+    @staticmethod
+    def enterPin(pin):
+        GenericPO.webDriver.findElementBy(FORM_PIN_FIELD, LocatorsType=LocatorsTypes.XPATH).send_keys(pin)
+
+    @staticmethod
+    def enterDate(date):
+        GenericPO.webDriver.findElementBy(FORM_DATE_FIELD, LocatorsType=LocatorsTypes.XPATH).send_keys(date)
+
+    @staticmethod
+    def chooseOptinTrue():
+        GenericPO.webDriver.findElementBy(FORM_OPTIN_TRUE, LocatorsType=LocatorsTypes.XPATH).click()
+
+    @staticmethod
+    def submitForm():
+        GenericPO.webDriver.findElementBy(FORM_SUBMIT_BUTTON, LocatorsType=LocatorsTypes.XPATH).click()
 # end of enter phone class
 
 # class EnterEmailPage(GenericPO):
