5afec5541ebab6a4d7635406992b06e34a835e75
Gal Ifrah <gal.ifrahh@gmail.com>
2019-01-04 17:32:38 +0200

    trying new config format

---
+++ App/Config.py
@@ -0,0 +1,11 @@
+class FmConfig(object):
+
+    COOKIES_POLICY_BTN = "//div[@class='continue ng-binding']"
+    NEW_SESSION = "newSession"
+    GET_ALL_SESSIONS = "getAllSessions"
+
+
+class MlConfig(object):
+    COOKIES_POLICY_BTN = "//div[@class='continue ng-binding__']"
+    NEW_SESSION = "newSession"
+    GET_ALL_SESSIONS = "getAllSessions"
