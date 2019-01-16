dc749fa01000eb5876b79f7edaa1a3ad760c0f0d
Gal <gal.i@mycheck.co.il>
2019-01-16 15:40:46 +0200

    add test classes init class

---
+++ SutTests/TestsClassesInit.py
@@ -0,0 +1,10 @@
+import unittest
+from App import PageObjects
+from App.PageObjects import *
+from Utils.ErrorHandler import ErrorsHandler
+
+from SutTests.ConnectTests import *
+from SutTests.WalletTests import *
+from SutTests.TestsDrifts import *
+from SutTests.HomeScreenTests import *
+from SutTests.MenuTests import *
